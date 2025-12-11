f9de18205954365e353d7b2981815b885ab11b3d
rhabegger <rhabegger@spark-login.chtc.wisc.edu>
2025-12-11 15:22:01 -0600

    New updated pgens from Wisc HPC

---
+++ myConfs/real_grav_conf.sh
@@ -0,0 +1,10 @@
+python3 configure.py \
+ --prob=realistic_grav_SN \
+ --nghost=4 \
+ --eos="adiabatic" \
+ --flux=lhlld \
+ -b \
+ -hdf5 \
+ -mpi \
+ -h5double \
+ --hdf5_path=/home/rhabegger/spack/var/spack/environments/athenaICM/.spack-env/view \
