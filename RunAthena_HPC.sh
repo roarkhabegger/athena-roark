3f138c36cd77462baf34c6d36caa1adac06408f6
Roark <roarkhabegger@gmail.com>
2026-03-18 11:41:17 -0500

    New RunAthena Script for CHTC HPC

--- RunAthena_HPC.sh
+++ RunAthena_HPC.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --mem-per-cpu=4000  #<- 256GB per node, 128 cores per node
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
+#SBATCH --mem-per-cpu=1000  #<- 256GB per node, 128 cores per node
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=64
 #SBATCH --partition=shared     
-#SBATCH --job-name=ath_make
-#SBATCH --time=0-24:00:00      # hh:mm:ss for the job
+#SBATCH --job-name=test2
+#SBATCH --time=24:00:00      # hh:mm:ss for the job
 #SBATCH -e slurm-%j.err
 #SBATCH -o slurm-%j.out
 
@@ -12,7 +12,5 @@ module reset # drop modules and explicitly load the ones needed
              # (good job metadata and reproducibility)
              # $WORK and $SCRATCH are now set
 spack env activate athenaICM
-make clean
-make clean
 
-srun ./athena -i athinput.cr_sne -t 0-23:50:00 > athena.01.log
+srun ./athena -i athinput.realistic_grav -t 23:50:00 > silcc.log
